15663ab7aa4ab10ad2051a617c42a4ca52cfcc0a
Xain447 <kzain3714@gmail.com>
2021-06-01 12:21:10 +0500

    Screen one added

---
+++ lib/screen_one.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ScreenOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Screen One "),
+      ),
+    );
+  }
+}
